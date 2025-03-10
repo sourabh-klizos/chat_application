8427d35b3775626cb701c62d7cf718b1103c98be
sourabh das <sourabh.das@klizos.com>
2025-03-10 18:08:31 +0530

    formated

--- app/utils/create_unique_group.py
+++ app/utils/create_unique_group.py
@@ -1,34 +1,15 @@
 from app.utils.logger_config import LOGGER
 
 
-# class ChatGroup:
-
-#     @staticmethod
-#     async def create_unique_group(user_a: str, user_b: str):
-#         """Creates a unique sorted group with a random string added."""
-#         try:
-#             if (user_a and user_b) and (user_b != "null"):
-#                 group: str = user_a + user_b
-#                 group = "".join(sorted(group))
-#                 return group
-#             return None
-#         except Exception as e:
-#             LOGGER.error(
-#                 "Error occurred while creating the unique group: %s",
-#                 str(e),
-#                 exc_info=True,
-#             )
-#             return None
-
-
 class ChatGroup:
 
     @staticmethod
     async def create_unique_group(user_a: str, user_b: str):
-        """Creates a unique sorted group using min/max."""
+        """Creates a unique sorted group with a random string added."""
         try:
             if (user_a and user_b) and (user_b != "null"):
-                group = min(user_a, user_b) + max(user_a, user_b)
+                group: str = user_a + user_b
+                group = "".join(sorted(group))
                 return group
             return None
         except Exception as e:
@@ -38,3 +19,22 @@ class ChatGroup:
                 exc_info=True,
             )
             return None
+
+
+# class ChatGroup:
+
+#     @staticmethod
+#     async def create_unique_group(user_a: str, user_b: str):
+#         """Creates a unique sorted group using min/max."""
+#         try:
+#             if (user_a and user_b) and (user_b != "null"):
+#                 group = min(user_a, user_b) + max(user_a, user_b)
+#                 return group
+#             return None
+#         except Exception as e:
+#             LOGGER.error(
+#                 "Error occurred while creating the unique group: %s",
+#                 str(e),
+#                 exc_info=True,
+#             )
+#             return None
